318c51cba783488a730f9858e2cd800a428465e5
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2020-12-02 12:44:16 +0000

    Add: CPP Solution_02 - Part One

---
+++ cpp_solutions/solution_02.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+auto read_puzzle_input(std::string filepath) -> std::vector<std::string>
+{
+    auto result = std::vector<std::string>(); 
+    std::ifstream file(filepath);
+    for(std::string line; std::getline(file, line);) { result.emplace_back(line); }
+    return result;
+}
+
+auto is_valid_password(const std::string& database_row) -> bool
+{
+    auto result = false;
+    auto find_hyphen_iterator = std::find(database_row.begin(), database_row.end(), '-');
+    auto find_space_iterator = std::find(database_row.begin(), database_row.end(), ' ');
+    auto find_colon_iterator = std::find(database_row.begin(), database_row.end(), ':');
+    auto lower_range = std::stoi(database_row.substr(0, find_hyphen_iterator - database_row.begin()));
+    auto upper_range = std::stoi(database_row.substr(1 + find_hyphen_iterator - database_row.begin(), 
+        find_space_iterator - database_row.begin() - 2));
+    auto desired_character = *(find_space_iterator + 1);
+    auto potential_password = database_row.substr(find_colon_iterator - database_row.begin() + 2);
+    auto character_frequency = std::count(potential_password.begin(), potential_password.end(), desired_character);
+    if(character_frequency >= lower_range && character_frequency <= upper_range) { result = true; }
+    return result;
+}
+
+auto main(int argc, char** argv) -> int
+{
+    auto answer = std::pair<int, int>();
+    auto corrupted_database = read_puzzle_input("../puzzle_input/day_02.txt");
+    auto valid_database_rows = std::vector<std::string>();
+    std::for_each(corrupted_database.begin(), corrupted_database.end(), 
+        [&](const auto& database_row) 
+        { 
+            if(is_valid_password(database_row)) 
+            { 
+                valid_database_rows.emplace_back(database_row); 
+            }
+        }
+    );
+    answer.first = valid_database_rows.size();
+    std::cout << "Answer Part One: " << answer.first << "\nAnswer Part Two: " << answer.second << "\n";
+    return 0;
+}
