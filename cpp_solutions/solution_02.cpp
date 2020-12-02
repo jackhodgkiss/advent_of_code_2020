7bd788544384e6e4a3143c84dfb214e85ec85de9
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2020-12-02 13:01:58 +0000

    Add: CPP Solution_02 - Part Two

--- cpp_solutions/solution_02.cpp
+++ cpp_solutions/solution_02.cpp
@@ -12,7 +12,7 @@ auto read_puzzle_input(std::string filepath) -> std::vector<std::string>
     return result;
 }
 
-auto is_valid_password(const std::string& database_row) -> bool
+auto is_valid_password(const std::string& database_row, const bool& use_toboggan_policy) -> bool
 {
     auto result = false;
     auto find_hyphen_iterator = std::find(database_row.begin(), database_row.end(), '-');
@@ -24,7 +24,20 @@ auto is_valid_password(const std::string& database_row) -> bool
     auto desired_character = *(find_space_iterator + 1);
     auto potential_password = database_row.substr(find_colon_iterator - database_row.begin() + 2);
     auto character_frequency = std::count(potential_password.begin(), potential_password.end(), desired_character);
-    if(character_frequency >= lower_range && character_frequency <= upper_range) { result = true; }
+    if(!use_toboggan_policy)
+    {
+        if(character_frequency >= lower_range && character_frequency <= upper_range) { result = true; }
+    }
+    else
+    {
+        auto character_at_lower = potential_password.at(lower_range - 1);
+        auto character_at_upper = potential_password.at(upper_range - 1);
+        if(character_at_lower != character_at_upper)
+        {
+            result = (desired_character == character_at_lower) 
+                ? true : (desired_character == character_at_upper) ? true : false;
+        }
+    }
     return result;
 }
 
@@ -32,17 +45,28 @@ auto main(int argc, char** argv) -> int
 {
     auto answer = std::pair<int, int>();
     auto corrupted_database = read_puzzle_input("../puzzle_input/day_02.txt");
-    auto valid_database_rows = std::vector<std::string>();
+    auto valid_sled_database_rows = std::vector<std::string>();
+    auto valid_toboggan_database_rows = std::vector<std::string>();
     std::for_each(corrupted_database.begin(), corrupted_database.end(), 
         [&](const auto& database_row) 
         { 
-            if(is_valid_password(database_row)) 
+            if(is_valid_password(database_row, false)) 
             { 
-                valid_database_rows.emplace_back(database_row); 
+                valid_sled_database_rows.emplace_back(database_row); 
             }
         }
     );
-    answer.first = valid_database_rows.size();
+    std::for_each(corrupted_database.begin(), corrupted_database.end(), 
+        [&](const auto& database_row) 
+        { 
+            if(is_valid_password(database_row, true)) 
+            { 
+                valid_toboggan_database_rows.emplace_back(database_row); 
+            }
+        }
+    );
+    answer.first = valid_sled_database_rows.size();
+    answer.second = valid_toboggan_database_rows.size();
     std::cout << "Answer Part One: " << answer.first << "\nAnswer Part Two: " << answer.second << "\n";
     return 0;
 }
