603f72414b539e5ce61bdccb3256f7b9498162f7
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2020-12-05 21:27:23 +0000

    Add: CPP Solution_05 - Part One

---
+++ cpp_solutions/solution_05.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+auto read_puzzle_input(std::string filepath) -> std::vector<std::string>
+{
+    auto result = std::vector<std::string>(); 
+    std::ifstream file(filepath);
+    for(std::string line; std::getline(file, line);) { result.emplace_back(line); }
+    return result;
+}
+
+auto decode_seat = [](const auto& seat)
+{
+    auto result = 0;
+    auto lower_upper_rows = std::pair<int, int>(0, 127);
+    auto lower_upper_columns = std::pair<int, int>(0, 7);
+    auto decoded_row_column = std::pair<int, int>(0, 0);
+    for(auto counter = 0; counter < seat.size(); counter++)
+    {
+        auto character = seat[counter];
+        if(counter < 7)
+        {
+            float row_range = lower_upper_rows.second - lower_upper_rows.first;
+            if(character == 'F') { lower_upper_rows.second -= std::ceil(row_range / 2); }
+            else { lower_upper_rows.first += std::ceil(row_range / 2); }
+        }
+        else
+        {
+            float column_range = lower_upper_columns.second - lower_upper_columns.first;
+            if(character == 'L') { lower_upper_columns.second -= std::ceil(column_range / 2); }
+            else { lower_upper_columns.first += std::ceil(column_range / 2); }
+        }
+    }
+    decoded_row_column = std::pair<int, int>(lower_upper_rows.first, lower_upper_columns.first);
+    result = decoded_row_column.first * 8 + decoded_row_column.second;
+    return result;
+};
+
+auto main(int argc, char** argv) -> int
+{
+    auto answer = std::pair<int, int>();
+    auto airline_seats = read_puzzle_input("../puzzle_input/day_05.txt");
+    auto decoded_seat_ids = std::vector<int>();
+    std::transform(airline_seats.begin(), airline_seats.end(), std::back_inserter(decoded_seat_ids), decode_seat);
+    answer.first = *std::max_element(decoded_seat_ids.begin(), decoded_seat_ids.end());
+    std::cout << "Answer Part One: " << answer.first << "\nAnswer Part Two: " << answer.second << "\n";
+    return 0;
+}
